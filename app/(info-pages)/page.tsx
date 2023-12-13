791bcb82383a1739c915a868334e5d9a5a821e2d
abratestenv <abrahampoog@gmail.com>
2023-12-13 20:50:18 +0800

    created hero section of home page

--- app/(info-pages)/page.tsx
+++ app/(info-pages)/page.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import styles from "./Page.module.scss";
 
 const Page = () => {
   return (
-    <section
-      style={{
-        background: "var(--secondary)",
-        height: "100svh",
-        color: "var(--light)",
-      }}
-    >
-      <Container>
-        <p>{"greeting"}</p>
+    <section className={styles.hero}>
+      <Container className={styles.container}>
+        <h1>Turning Ideas into Reality</h1>
+        <p>
+          Discover tailored software solutions for every industry. Elevate your
+          business with high-value technology and innovation.
+        </p>
+        <div>
+          <Button variant="primary">Get in Touch</Button>
+          <Button variant="secondary">Learn More</Button>
+        </div>
       </Container>
     </section>
   );
