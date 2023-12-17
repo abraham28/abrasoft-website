3d28601324e958d3cb9304f97a7e2c40a09c5593
abratestenv <abrahampoog@gmail.com>
2023-12-17 16:39:00 +0800

    replaced images and added wash u want in client reviews

--- app/(info-pages)/components/ClientsSection/index.tsx
+++ app/(info-pages)/components/ClientsSection/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Carousel, CarouselItem, Container } from "react-bootstrap";
 import styles from "./ClientsSection.module.scss";
 import Image from "next/image";
-import StGabrielleLogo from "@/public/images/st-gabrielle-logo.webp";
+import StGabrielleLogo from "@/public/images/st-gabrielle-hrs-logo-with-stroke.webp";
+import WashUWantLogo from "@/public/images/wash-u-want-logo-with-stroke.webp";
 import Link from "next/link";
 
 const ClientsSection = () => {
@@ -12,38 +13,76 @@ const ClientsSection = () => {
       <Container className={styles.container}>
         <h4>Client Review</h4>
         <h2>We&apos;d love to serve you too</h2>
-        <div className={styles.details}>
-          <div className={styles.detailsText}>
-            <h4 className={styles.detailTitle}>
-              {"St. Gabrielle HR Solutions"}
-            </h4>
-            <p>
-              visit their website on:&nbsp;
-              <Link href="https://stgabriellehrs.com/" target="_blank">
-                {"stgabriellehrs.com"}
-              </Link>
-            </p>
-            <blockquote>
-              Our experience with Abrasoft in building our website has been
-              exceptional. The web development team demonstrated expertise in
-              crafting a site that perfectly aligns with our brand and business
-              objectives. The sleek, user-centric design coupled with seamless
-              application form integration has significantly enhanced our online
-              presence. Their commitment to SEO optimization has boosted our
-              visibility. Overall, a professional and dedicated team that we
-              highly recommend for web development projects.
-            </blockquote>
-          </div>
-          <div className={styles.imgContainer}>
-            <Image
-              src={StGabrielleLogo}
-              alt="St Gabrielle HRS Logo"
-              height={250}
-              width={500}
-              className="d-inline-block align-text-top"
-            />
-          </div>
-        </div>
+        <Carousel controls={false}>
+          <CarouselItem>
+            <div className={styles.details}>
+              <div className={styles.detailsText}>
+                <h4 className={styles.detailTitle}>
+                  {"St. Gabrielle HR Solutions"}
+                </h4>
+                <p>
+                  visit their website on:&nbsp;
+                  <Link href="https://stgabriellehrs.com/" target="_blank">
+                    {"stgabriellehrs.com"}
+                  </Link>
+                </p>
+                <blockquote>
+                  Our experience with Abrasoft in building our website has been
+                  exceptional. The web development team demonstrated expertise
+                  in crafting a site that perfectly aligns with our brand and
+                  business objectives. The sleek, user-centric design coupled
+                  with seamless application form integration has significantly
+                  enhanced our online presence. Their commitment to SEO
+                  optimization has boosted our visibility. Overall, a
+                  professional and dedicated team that we highly recommend for
+                  web development projects.
+                </blockquote>
+              </div>
+              <div className={styles.imgContainer}>
+                <Image
+                  src={StGabrielleLogo}
+                  alt="St Gabrielle HRS Logo"
+                  height={250}
+                  width={500}
+                  style={{ height: "auto", maxWidth: 250 }}
+                  className="d-inline-block align-text-top"
+                />
+              </div>
+            </div>
+          </CarouselItem>
+          <CarouselItem>
+            <div className={styles.details}>
+              <div className={styles.detailsText}>
+                <h4 className={styles.detailTitle}>{"Wash U Want"}</h4>
+                <p>
+                  visit their website on:&nbsp;
+                  <Link href="https://washuwant.com/" target="_blank">
+                    {"washuwant.com"}
+                  </Link>
+                </p>
+                <blockquote>
+                  Abrasoft delivered exceptional results for our laundry shop.
+                  Their team crafted a sleek landing page that perfectly aligns
+                  with our brand. The seamless integration of their laundry
+                  management SAAS has streamlined our operations, and their
+                  commitment to SEO optimization has boosted our online
+                  visibility. Highly recommend their professional and dedicated
+                  team for web development projects.
+                </blockquote>
+              </div>
+              <div className={styles.imgContainer}>
+                <Image
+                  src={WashUWantLogo}
+                  alt="Wash U Want Logo"
+                  height={250}
+                  width={500}
+                  style={{ height: "auto", maxWidth: 250 }}
+                  className="d-inline-block align-text-top"
+                />
+              </div>
+            </div>
+          </CarouselItem>
+        </Carousel>
       </Container>
     </section>
   );
